6fcc4fc873472991f2ad664be0fbabfdac977766
Rafael <borgesrafael935@gmail.com>
2020-06-11 18:13:32 -0300

    Dúvida 01

--- ProjetoED/src/Controller/Ordenacao.java
+++ ProjetoED/src/Controller/Ordenacao.java
@@ -1,9 +1,16 @@
 package Controller;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+import Entity.Leito;
+
 public class Ordenacao
 {
-	public static void main(String[] args) 
+	public void bubblesort(List<Leito> lst)
 	{
-		
+		ArrayList<String> al = new ArrayList<String>();
+		Collections.sort(lst, Collections.reverseOrder());
 	}
 }

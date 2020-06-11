cc4639c327ec8fa6863b72ac0e45ac60dbc5ccc0
Rafael <borgesrafael935@gmail.com>
2020-06-11 15:39:11 -0300

    01

--- ProjetoED/src/Main/Principal.java
+++ ProjetoED/src/Main/Principal.java
@@ -1,10 +1,9 @@
 package Main;
 
-public class Principal {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+public class Principal 
+{
+	public static void main(String[] args)
+	{
 
 	}
-
 }

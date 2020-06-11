e819ee3fbb7b32a281298073df553552c6af9b6b
Rafael <borgesrafael935@gmail.com>
2020-06-11 15:49:09 -0300

    Criação Classe Leito

--- ProjetoED/src/Controller/teste.java
+++ ProjetoED/src/Controller/teste.java
@@ -2,5 +2,5 @@ package Controller;
 
 public class teste 
 {
-
+	
 }
---
+++ ProjetoED/src/Entity/Leito.java
@@ -0,0 +1,65 @@
+package Entity;
+
+import java.util.Date;
+
+public class Leito 
+{
+	private Date data; /* Data no formato YYYY-MM-DD */
+	private String nomedistrito; /* DRS ou região da Grande São Paulo */
+	private int totalcovidleito; /* Total de Leitos de UTI Destinados para COVID-19 no dia */
+	private Long populacao;/* População da DRS ou região da Grande São Paulo (Fonte: SEADE) */
+	private Double leitospc;/* Leitos Covid-19 UTI por 100 mil habitantes (total_covid_uti / pop) */
+	private int internacoes7d;/* Número de novas internações de pacientes confirmados ou com suspeita de COVID-19 nos últimos 7 dias */
+	private int internacoes7di;/* Número de novas internações de pacientes confirmados ou com suspeita de COVID-19 nos 7 dias anteriores */
+	private Double internacoes7v7;/* Variação no número de novas internações ((internacoes_7d - internacoes_7d_l) / internacoes_7d_l) */
+	public Date getData() {
+		return data;
+	}
+	public void setData(Date data) {
+		this.data = data;
+	}
+	public String getNomedistrito() {
+		return nomedistrito;
+	}
+	public void setNomedistrito(String nomedistrito) {
+		this.nomedistrito = nomedistrito;
+	}
+	public int getTotalcovidleito() {
+		return totalcovidleito;
+	}
+	public void setTotalcovidleito(int totalcovidleito) {
+		this.totalcovidleito = totalcovidleito;
+	}
+	public Long getPopulacao() {
+		return populacao;
+	}
+	public void setPopulacao(Long populacao) {
+		this.populacao = populacao;
+	}
+	public Double getLeitospc() {
+		return leitospc;
+	}
+	public void setLeitospc(Double leitospc) {
+		this.leitospc = leitospc;
+	}
+	public int getInternacoes7d() {
+		return internacoes7d;
+	}
+	public void setInternacoes7d(int internacoes7d) {
+		this.internacoes7d = internacoes7d;
+	}
+	public int getInternacoes7di() {
+		return internacoes7di;
+	}
+	public void setInternacoes7di(int internacoes7di) {
+		this.internacoes7di = internacoes7di;
+	}
+	public Double getInternacoes7v7() {
+		return internacoes7v7;
+	}
+	public void setInternacoes7v7(Double internacoes7v7) {
+		this.internacoes7v7 = internacoes7v7;
+	}
+	
+	
+}

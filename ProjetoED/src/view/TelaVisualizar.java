18d2fdec205bce26b15c83d661709966901c2bad
fellipea <fellipealves05@hotmail.com>
2020-06-12 19:38:20 -0300

    a

--- ProjetoED/src/view/TelaVisualizar.java
+++ ProjetoED/src/view/TelaVisualizar.java
@@ -24,7 +24,7 @@ public class TelaVisualizar extends JFrame {
 	public TelaVisualizar() {
 		setVisible(true);
 		setTitle("Visualizar Dados");
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setDefaultCloseOperation(JFrame.HIDE_ON_CLOSE);
 		setBounds(100, 100, 250, 205);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
